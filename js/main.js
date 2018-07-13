c536147bd6d368fb3d0dfd1bf2493d51725387e5
Mikhail Karan <info@digitaldynasty.ca>
2018-07-13 10:02:30 -0400

    -added lightbox -added home section animal

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 $( document ).ready(function() {
     
-    $(window).on('activate.bs.scrollspy', function (event) {
+    $(window).on('activate.bs.scrollspy', function (event) { //show and hide navbars based on if on the home page or not
         var navId = $('.list-group .active').attr('href');
         if (navId != "#home"){
-            //$('.navigation, .mobile-nav').css('visibility', 'visible');
             $('.navigation').css('top', '0');
             $('.mobile-nav').css('bottom', '0');
         } else {
-            //$('.navigation, .mobile-nav').css('visibility', 'hidden');
             $('.navigation').css('top', '-53px');
             $('.mobile-nav').css('bottom', '-50px');
         }
       });
 
-    $('.portfolio-item').hover(function(){
+    $('.portfolio-item').hover(function(){ //animations for portoflio items
         $(this).find('div.splitter').css('border-bottom', '0 solid #6534ff');
     }, function() {
         $(this).find('div.splitter').css('border-bottom', '140px solid #6534ff');
@@ -23,7 +21,7 @@ $( document ).ready(function() {
 $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
-    $('html, body').animate({
+    $('html, body').animate({ //smooth scroll
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
 });
